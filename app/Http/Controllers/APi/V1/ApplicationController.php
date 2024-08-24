f82def435d29fee8e0e8f3ac640121183bb12932
Wello_Maqar <dev.waleed.atef89@gmail.com>
2024-08-24 18:20:53 +0300

    Added controller for application Added request for creating application

---
+++ app/Http/Controllers/APi/V1/ApplicationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\APi\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApplicationController extends Controller
+{
+    //
+}
---
+++ app/Http/Requests/Api/V1/ApplicationCreateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ApplicationCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'job_id' => 'required|exists:jobs,id',
+            'user_id' => 'required|exists:users,id',
+            'name' => 'required',
+            'email' => 'required|email',
+            'mobile' => 'required|min:11|max:11|regex:patterns/0[0-9]{9}/',
+            'skills' => 'required',
+            'linkedin' => 'required',
+            'plan' => 'required',
+        ];
+    }
+}
